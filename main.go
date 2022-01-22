39f28be3190266df8c96becd73396cee038e60cc
Miguel Alexandre <mumbleinout@gmail.com>
2022-01-22 18:22:40 +0000

    Fix identation (default tabs)

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "mockServer/src/parser"
-    "mockServer/src/server"
+	"fmt"
+	"mockServer/src/parser"
+	"mockServer/src/server"
 )
 
 const (
-    name   = "MockServer"
-    author = "Miguel Alexandre"
-    at     = "@2022"
+	name   = "MockServer"
+	author = "Miguel Alexandre"
+	at     = "@2022"
 )
 
 var (
-    port        = ":8080"
-    defaultPath = "endpoints.json"
+	port        = ":8080"
+	defaultPath = "endpoints.json"
 )
 
 func main() {
-    fmt.Printf("%s made with ❤️ by %s %s \n", name, author, at)
-    fmt.Printf("Start server at %s\n", port)
-    server.RunServer(port, parser.LoadEndpointsFromContext(defaultPath))
+	fmt.Printf("%s made with ❤️ by %s %s \n", name, author, at)
+	fmt.Printf("Start server at %s\n", port)
+	server.RunServer(port, parser.LoadEndpointsFromContext(defaultPath))
 }
